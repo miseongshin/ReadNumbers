4fe1650e6c74bf1db4fcd1699bcfffef55e386d6
miseongshin <miseong1028@gmail.com>
2017-12-31 23:42:53 +0900

    js 수정

--- tellmethisnumber.js
+++ tellmethisnumber.js
@@ -6,17 +6,12 @@ var tellmethisnumber = {
 		default : "#e7adad"
 	}
 	,init : function(){
-		$(".input").mousedown(function(){
-			console.log("움짐");
-		});
+			console.log("init");
 		/*$(".output").hide();*/
 	}
-
-	
 };
-
-
 
 $(document).ready(function(){
 	tellmethisnumber.init();
 });
+
