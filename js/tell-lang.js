8df53e680f8d791a03e13e37dc342f70c2aea7ed
miseongshin <miseong1028@gmail.com>
2018-02-17 19:45:34 +0900

    기본기능 완성. 모바일키테스트1

--- js/tell-lang.js
+++ js/tell-lang.js
@@ -24,40 +24,43 @@ var $tellLang = {
         langJp: ["일본어", "Japanese", "日本", "日本语"],
         langZh: ["중국어", "Chinese", "中国", "中国语"],
         footerText: [
-            "이 사이트는 <a href='https://howsecureismypassword.net'>[내 비밀번호는 얼마나 안전할까?]</a>의 복제 사이트 입니다." +
-            "<br>학습을 위한 <a href='https://github.com/CloneProject'>클론 프로젝트</a>의 일부로 만들었습니다. " +
-            "<br><a href='https://miseongshin.github.io/'>신 미성</a> 제작 2017년 12월 18일 - 2018년 2월 24일",
-            "This site is a clone site of <a href='https://howsecureismypassword.net'>HOW SECURE IS MY PASSWORD?</a>." +
-            "<br>It was created as part of a <a href='https://github.com/CloneProject'>CLONE PROJECT</a> for a studing." +
-            "<br>Created by <a href='https://miseongshin.github.io/'> miseongShin</a> in Dec 18, 2017 - Fab 24, 2018",
-            "このサイトは、<a href='https://howsecureismypassword.net'>[パスワードはどのように安全か？]</a>の複製サイトです。" +
-            "<br>学習のための<a href='https://github.com/CloneProject'>クローンプロジェクト</a>の一部として作成しました。" +
-            "<br><a href='https://miseongshin.github.io/'>申美星</a>製作2017年12月18日 - 2018年2月24日",
-            "这个网站是<a href='https://howsecureismypassword.net'>安全是我的密码?</a>的克隆网站。" +
-            "<br>它是作为<a href='https://github.com/CloneProject'> CLONE PROJECT </a>的一部分创建的，用于学习。" +
-            "<br>在2017年12月18日 - 2018年1月18日由<a href='https://miseongshin.github.io/'> 申美星 </a>创建"
+        "이 사이트는 <a href='https://howsecureismypassword.net'>[내 비밀번호는 얼마나 안전할까?]</a>의 복제 사이트 입니다." +
+        "학습을 위한 <a href='https://github.com/CloneProject'>클론 프로젝트</a>의 일부로 만들었습니다. " +
+        "<br><a href='https://miseongshin.github.io/'>신 미성</a> 제작 2017년 12월 18일 - 2018년 2월 24일"
+        ,
+        "This site is a clone site of <a href='https://howsecureismypassword.net'>HOW SECURE IS MY PASSWORD?</a>." +
+        "It was created as part of a <a href='https://github.com/CloneProject'>CLONE PROJECT</a> for a studing." +
+        "<br>Created by <a href='https://miseongshin.github.io/'> miseongShin</a> in Dec 18, 2017 - Fab 24, 2018"
+        ,
+        "このサイトは、<a href='https://howsecureismypassword.net'>[パスワードはどのように安全か？]</a>の複製サイトです。" +
+        "学習のための<a href='https://github.com/CloneProject'>クローンプロジェクト</a>の一部として作成しました。" +
+        "<br><a href='https://miseongshin.github.io/'>申美星</a> 製作2017年12月18日 - 2018年2月24日"
+        ,
+        "这个网站是<a href='https://howsecureismypassword.net'>安全是我的密码?</a>的克隆网站。" +
+        "它是作为<a href='https://github.com/CloneProject'> CLONE PROJECT </a>的一部分创建的，用于学习。" +
+        "<br>在2017年12月18日 - 2018年1月18日由<a href='https://miseongshin.github.io/'> 申美星 </a>创建"
         ]
     },
     langUnitArr: {
-    	0 : ["","만","억","조","경","해","서","양","구","간","정","재","극","항아사","아승기","나유타","불가사의"],
-        1 : ["","thousand","억","조","경","해","서","양","구","간","정","재","극","항아사","아승기","나유타","불가사의"],
-        2 : ["","만","억","조","경","해","서","양","구","간","정","재","극","항아사","아승기","나유타","불가사의"],
-        3 : ["","만","억","조","경","해","서","양","구","간","정","재","극","항아사","아승기","나유타","불가사의"]
+    	0 : ["","만","억","조","경","해","서","양","구","간","정","재","극","항아사","아승기","나유타","불가사의","무량대수"],
+        1 : ["","Thousand","Million","Billion","Trillion","Quadrillion","Quintillion","Sextillion","Septillion","Octillion","Nonillion","Decillion","Undecillion","Duodecillion","Tredecillion","Quattuordecillion","Quindecillion","Sexdecillion","Septendecillion","Octodecillion","Novemdecillion","Vigintillion","Centillion"],
+        2 : ["","万","億","兆","京","垓","𥝱","穰","溝","澗","正","載","極","恒河沙","阿僧祇","那由他","不可思議","無量大数"],
+        3 : ["","万","亿","兆","京","垓","秭","穰","溝","澗","正","載","極","恒河沙","阿僧祇","那由他","不可思議","無量大數"]
     },
     lang1Arr : {
-    	0 : ["","일","이","삼","사","오","육","칠","팔","구" ],
-        1 : ["","one","two","three","four","five","six","seven","eight","nine", "ten", "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen" ],
-        2 : ["","一","二","三","四","五","六","七","八","九" ],
-        3 : ["","일","이","삼","사","오","육","칠","팔","구" ]
+    	0 : ["영","일","이","삼","사","오","육","칠","팔","구" ],
+        1 : ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine", "Ten", "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen" ],
+        2 : ["零","一","二","三","四","五","六","七","八","九" ],
+        3 : ["〇","一","二","三","四","五","六","七","八","九" ]
     },
     lang10Arr : {
     	0 : ["","십","백","천"],
-        1 : ["","ty","hundred","thousand"],
-        2 : ["","십","백","천"],
-        3 : ["","십","백","천"] 
+        1 : ["","ty","Hundred","Thousand"],
+        2 : ["","十","百","千"],
+        3 : ["","十","百","千"] 
     },
     lang20Arr : {
-        1 : ["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+        1 : ["","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"]
     },
     bindAll: function() {
 
@@ -80,36 +83,30 @@ var $tellLang = {
         var comment1 = "";
         var comment2 = "";
         if (num == 0 ) {
-           imagePath = "./image/title/number-title-header-ko.png";
+         imagePath = "./image/title/number-title-header-ko.png";
 
-        } else {
-            imagePath = "./image/title/number-title-header-en.png";
+     } else {
+        imagePath = "./image/title/number-title-header-en.png";
 
-        }
-
-        if (num ==1 ) {
-            comment1 = "in";
-        }else{
-            comment1 = "";
-        }
-        $("header .comment1").text(comment1);
-        $("header .comment2").text($tellLang.info.comment[num]);
-        $(".title-image img").attr('src',imagePath);
-        $(".korean").text($tellLang.info.langKo[num]);
-        $(".english").text($tellLang.info.langEn[num]);
-        $(".japanese").text($tellLang.info.langJp[num]);
-        $(".chinese").text($tellLang.info.langZh[num]);
-
-        /*document.getElementById("select-site-lang").innerHTML = $tellLang.info.selSiteLang[num];*/
-        /*document.getElementById("select-read-lang").innerHTML = $tellLang.info.selReadLang[num];*/
-        /*document.getElementById("tellme-title").textContent = $tellLang.info.tellNumTitle[num];*/
-   /*     document.getElementsByTagName("title")[0].text = $tellLang.info.tellNumTitle[num];
-        document.getElementById("ENTER").placeholder = $tellLang.info.enterNum[num];
-        document.getElementsByTagName("footer")[0].innerHTML = $tellLang.info.footerText[num];
-*/
-
-    },
-    init: function() {
-        $tellLang.setLanguage();
     }
+
+    if (num ==1 ) {
+        comment1 = "in";
+    }else{
+        comment1 = "";
+    }
+    $("header .comment1").text(comment1);
+    $("header .comment2").text($tellLang.info.comment[num]);
+    $(".title-image>img").attr('src',imagePath);
+    $(".korean").text($tellLang.info.langKo[num]);
+    $(".english").text($tellLang.info.langEn[num]);
+    $(".japanese").text($tellLang.info.langJp[num]);
+    $(".chinese").text($tellLang.info.langZh[num]);
+    $("footer").html($tellLang.info.footerText[num]);
+
+
+},
+init: function() {
+    $tellLang.setLanguage();
+}
 }
--- js/tell.js
+++ js/tell.js
@@ -1,157 +1,162 @@
 var $tell = {
-    color: {
-        darks: ["376a82", "c23333", "db7d3a", "378241"],
-        lights: ["afc3cd", "e7adad", "f1cbb0", "afcdb3"]
-    },
-    bindAll: function() {
-    	$(document).on("keyup","input[type=number]" ,function(e){
-    		if ($tell.isNotNumKey(event)) {
-                console.log("숫자아님");
-                ENTER.value = ENTER.value.replace(/[^0-9]/g, "");
+	color: {
+		darks: ["376a82", "c23333", "db7d3a", "378241"],
+		lights: ["afc3cd", "e7adad", "f1cbb0", "afcdb3"]
+	},
+	bindAll: function() {
+		$(document).on("keyup", "input[type=number]", function(e) {
+			if ($tell.isNotNumKey(event)) {
+				ENTER.value = ENTER.value.replace(/[^0-9]/g, "");
 
-                return false;
-            }
-    		$tell.changedNum();
-    	});
+				return false;
+			}
+			$tell.changedNum();
+		});
 
-    	$(document).on("change",".1st-lang-select",function(e){
-            var siteLangNum = $(".1st-lang-select option:selected").val();
-            $tell.setUpSiteView(siteLangNum);
-        });
+		$(document).on("change", ".1st-lang-select", function(e) {
+			var siteLangNum = $(".1st-lang-select").val();
+			$tell.setUpSiteView(siteLangNum);
+			$tell.changedNum();
+		});
 
-        $(document).on("change",".2st-lang-select",function(e){
-        	var siteLangNum = $(".1st-lang-select option:selected").val();
-            $tell.setUpSiteView(siteLangNum);
-        });
-/*        var ENTER = document.getElementById("ENTER");
-        ENTER.onkeyup = function(event) {
-            if ($tell.isNotNumKey(event)) {
-                console.log("숫자아님");
-                ENTER.value = ENTER.value.replace(/[^0-9]/g, "");
-                return false;
-            }
+		$(document).on("change", ".2st-lang-select", function(e) {
+			$tell.changedNum();
+		});
 
-            if($tell.isNotReadNumPatton){
+		$tell.init();
+	},
+	changedNum: function() {
+		enterLength = $("#ENTER").val().trim().length;
+		if (enterLength == 0) {
+			$("main .title-image img").show();
+			$("header .title-image img").hide();
+		} else {
+			$("main .title-image img").hide();
+			$("header .title-image img").show();
+		}
+		$tell.setColor(enterLength)
+		$tell.getEnterOutNum();
+	},
+	getEnterOutNum: function() {
+		var enterText = $("#ENTER").val();
+		var enterArr = $tell.getEnterArr(3, enterText);
+		$(".out-number h1").html(enterArr.toString());
 
-            }
+		var lang1 =  $(".1st-lang-select").val();
+		var readArr = $tell.getReadArr($tellLang.langUnitSize[lang1], enterText, lang1);
+		var readText = $tell.getArrToText(readArr);
+		$(".out-read-lang1 h1").html(readText);
 
+		var lang2 = $(".2nd-lang-select option:selected").val();
+		var readArr2 = $tell.getReadArr($tellLang.langUnitSize[lang2], enterText, lang2);
+		var readText2 = $tell.getArrToText(readArr2);
+		$(".out-read-lang2 h1").html(readText2);
+	},
+	getArrToText: function(readArr) {
+		var readText = "";
+		for (var i = readArr.length - 1; i >= 0; i--) {
+			readText += readArr[readArr.length - 1 - i] + " ";
+		}
+		return readText;
+	},
+	getReadArr: function(num, enterText, lang) {
+		if (lang != 0 && lang != 1 && lang != 2 && lang != 3) {
+			alert("not allowed Language");
+			return false;
+		}
+		var langArr = new Array();
 
-            $tell.changedNum();
-        }
-       document.getElementById("selectSiteLang").addEventListener("select", $tell.setSiteLang(), false);
-*/
-        $tell.init();
-    },
-    changedNum: function() {
-            $tell.getEnterOutNum();
-	        $("main .title-image").hide();
-    	    $("header .title-image").show();
+		if (parseInt(enterText) ==0) {
+			langArr[0] = $tellLang.lang1Arr[lang][0];
+			return langArr;
+		}
+		var enterArr = $tell.getEnterArr(num, enterText);
 
-            var inputNum = parseInt(document.getElementById("ENTER").value.trim());
-    },
-    getEnterOutNum: function() {
-        var enterText = $("#ENTER").val();
-        var enterArr = $tell.getEnterArr(3, enterText);
-        $(".out-number h1").html(enterArr.toString());
-        var lang1 = 0;
-        var readArr = $tell.getReadArr($tellLang.langUnitSize[lang1], enterText, lang1);
-        var readText = $tell.getArrToText(readArr);
-         $(".out-read-lang1 h1").html(readText);
-        var lang2 = $(".2nd-lang-select option:selected").val();
-        var readArr2 = $tell.getReadArr($tellLang.langUnitSize[lang2], enterText, lang2);
-        var readText2 = $tell.getArrToText(readArr2);
-        
-        $(".out-read-lang2 h1").html(readText2);
-    },
-    getArrToText : function(readArr){
-        var readText = "";
-        for (var i = readArr.length - 1; i >= 0; i--) {
-            readText += readArr[readArr.length - 1-i] +" ";
-        }
-        return readText;
-    },
-    getReadArr : function(num, enterText, lang){
-        var enterArr = $tell.getEnterArr(num, enterText);
-        var langArr = new Array(); 
-
+        //영어
         if (lang == $tellLang.lang['en']) {
         	var space = " ";
         	var plural = "s";
-	        for (var i =  enterArr.length - 1; i >= 0; i--) {
-    	        var inputNum = enterArr[i];
-            	var readUnit = "";
-            	var remainingNum = "";
-	            //100
-    	        if (inputNum.length ==3) {
-        	    	var num100 = inputNum.substring(0,1);
-            		remainingNum = inputNum.substring(1,3);
-            		if (num100 !=0) {
-            			readUnit += space + $tellLang.lang1Arr[lang][num100];
-            			readUnit += space + $tellLang.lang10Arr[lang][2];
-            		} 
-            		if (num100 != 0 && num100 != 1) {
-            			readUnit += plural;
-            		} 
-            	}else{
-            		remainingNum = inputNum;
-            	}
+        	for (var i = enterArr.length - 1; i >= 0; i--) {
+        		var inputNum = enterArr[i] + "";
+        		var readUnit = "";
+        		var remainingNum = "";
 
-            	console.log("십의자리",remainingNum);
-            	var num10 = parseInt(remainingNum);
-            	if (num10 <=19) {
-            		readUnit += space +$tellLang.lang1Arr[lang][num10];
-            			console.log( readUnit);
-            	} else {
-            		if ((num10 / 10) > 1 ) {
-            		var tenSeat = Math.floor(num10 / 10); 
-            			readUnit += space + $tellLang.lang20Arr[lang][tenSeat];
-            			if ( i != enterArr.length - 1) {
-            				readUnit += space;
-            			} 
-            		}
-            		console.log("num10 % 10",num10 % 10);
-            		if ((num10 % 10) > 0 ) {
-             			readUnit += space + $tellLang.lang1Arr[lang][(num10 % 10)];
-            		} 
-            	} 
-            	           	
-            	if (readUnit.length !=0) {
-            		readUnit += $tellLang.langUnitArr[lang][enterArr.length - i-1];
-            		if ((readUnit !=  $tellLang.lang1Arr[lang][1]) &&( i != enterArr.length - 1))
-            		{
-            			readUnit += plural;
-            		} 
-            	}
-            	langArr[i]=readUnit;
-            }
-
-        	return langArr;
-        } 
-
-        for (var i =  enterArr.length - 1; i >= 0; i--) {
-            var inputNum = enterArr[i];
-            var inputNumLength = inputNum.length;
-            var readUnit = "";
-            for (var j = inputNumLength - 1; j >= 0; j--) {
-                var num = inputNum.substring(inputNumLength-(j+1),inputNumLength-j);
-                if ( num != 0 ) {
-                	if((j == 0 || num != 1 ) || lang ==3 ){
-                		readUnit += $tellLang.lang1Arr[lang][num];
-                	}
-		            readUnit += $tellLang.lang10Arr[lang][j];
+                //3자리 수로 저장
+                if (inputNum.length == 1) {
+                	inputNum = "00" + inputNum;
+                } else if (inputNum.length == 2) {
+                	inputNum = "0" + inputNum;
                 }
-            }
-            if (readUnit.length !=0) {
-            	readUnit += $tellLang.langUnitArr[lang][enterArr.length - i-1];
-            }
-            langArr[i]=readUnit;
-        }
-        return langArr;
 
-    },
+                //100
+                var num100 = inputNum.substring(0, 1);
+                if (num100 != 0) {
+                	readUnit += space + $tellLang.lang1Arr[lang][num100];
+                	readUnit += space + $tellLang.lang10Arr[lang][2];
+                	if (num100 != 1) {
+                		readUnit += plural+space;
+                	}
+                }
+
+                //10
+                var num10 = inputNum.substring(1, 3);
+                if ( 0 < num10 && num10 <= 19) {
+                	readUnit += space + $tellLang.lang1Arr[lang][parseInt(num10)] + space;
+                } else {
+                	var tenthDigit = num10 / 10;
+                	var FirstDigit = num10 % 10;
+                	if (tenthDigit > 1) {
+                		var tenSeat = Math.floor(num10 / 10);
+                		readUnit += space + $tellLang.lang20Arr[lang][tenSeat];
+                	}
+
+                	if (tenthDigit > 0 && FirstDigit > 0) {
+                		readUnit +="-"; 
+                	} 
+
+                	if (FirstDigit > 0) {
+                		readUnit += $tellLang.lang1Arr[lang][(num10 % 10)]+ space;
+                	}
+                }
+
+                //units  	
+                if (readUnit.length != 0) {
+                	readUnit += $tellLang.langUnitArr[lang][enterArr.length - i - 1];
+                	if ((readUnit != $tellLang.lang1Arr[lang][1]) && (i != enterArr.length - 1)) {
+                		readUnit += plural;
+                	}
+                }
+                langArr[i] = readUnit;
+
+           }
+
+      } else {
+            //한중일 
+            for (var i = enterArr.length - 1; i >= 0; i--) {
+            	var inputNum = enterArr[i];
+            	var inputNumLength = inputNum.length;
+            	var readUnit = "";
+            	for (var j = inputNumLength - 1; j >= 0; j--) {
+            		var num = inputNum.substring(inputNumLength - (j + 1), inputNumLength - j);
+            		if (num != 0) {
+            			if ((j == 0 || num != 1) || lang == 3) {
+            				readUnit += $tellLang.lang1Arr[lang][num];
+            			}
+            			readUnit += $tellLang.lang10Arr[lang][j];
+            		}
+            	}
+            	if (readUnit.length != 0) {
+            		readUnit += $tellLang.langUnitArr[lang][enterArr.length - i - 1];
+            	}
+            	langArr[i] = readUnit;
+            }
+       }
+
+       return langArr;
+  },
     getEnterArr: function(num, enterText) { //1234567
-        $tell.checkNumKey;
-        var enterArr = new Array();
+    	$tell.checkNumKey;
+    	var enterArr = new Array();
         var enterLength = enterText.length; //7
         var maxNum = Math.ceil(enterLength / num); //3
         var tempEndNum = enterLength % num; //1
@@ -159,84 +164,84 @@ var $tell = {
         var startNum;
         var endNum;
         for (var i = maxNum; i >= 1; i--) {
-            if (i == maxNum) {
+        	if (i == maxNum) {
                 startNum = 0; //0
                 endNum = tempEndNum == 0 ? num : tempEndNum; //1
-            } else {
+           } else {
                 startNum = endNum; //3
                 endNum = startNum + num; //6
-            }
-            enterArr[maxNum - i] = enterText.substring(startNum, endNum);
-        }
-        return enterArr;
-    },
-    isNotNumKey: function(event) {
-        event = event || window.event;
-        var keyID= $tell.getKeyNum(event);
+           }
+           enterArr[maxNum - i] = enterText.substring(startNum, endNum);
+      }
+      return enterArr;
+ },
+ isNotNumKey: function(event) {
+ 	event = event || window.event;
+ 	var keyID = $tell.getKeyNum(event);
 
-        if ((keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8) {
-            return false;
-        } else {
-            return true;
-        }
-    },
-    getKeyNum : function(event){
-        var keyID = (event.which) ? event.which : event.keyCode;
-        //console.log("keyID", keyID);
+ 	if ((keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8) {
+ 		return false;
+ 	} else {
+ 		return true;
+ 	}
+ },
+ getKeyNum: function(event) {
+ 	var keyID = (event.which) ? event.which : event.keyCode;
+        $(".mobile-space").text($(".mobile-space").text()+"|"+keyID);
         return keyID;
-    },
-    setResultView: function() {
-/*        document.getElementsByClassName("output")[0].style.display = 'block';
-        document.getElementsByClassName("output")[0].style.display = 'block';
-        document.getElementsByClassName("other-section")[0].style.display = 'block';*/
-    },
-    setColor: function(length) {
-        var darkColor;
-        var lightColor;
+   },
+   setResultView: function() {
+/*          document.getElementsByClassName("output")[0].style.display = 'block';
+          document.getElementsByClassName("output")[0].style.display = 'block';
+          document.getElementsByClassName("other-section")[0].style.display = 'block';*/
+     },
+     setColor: function(length) {
+     	var darkColor;
+     	var lightColor;
 
-        if (length < 4) {
-            darkColor = "#" + $tell.color.darks[length];
-            lightColor = "#" + $tell.color.lights[length];
+     	if (length < 4) {
+     		darkColor = "#" + $tell.color.darks[length];
+     		lightColor = "#" + $tell.color.lights[length];
 
-        } else {
+     	} else {
 
-        }
+     	}
 
-        /*console.log(darkColor ," | ",lightColor);*/
-/*        document.getElementsByClassName("dark-color")[0].style.backgroundColor = darkColor;
-        document.getElementsByClassName("light-color")[0].style.backgroundColor = lightColor;
-        document.getElementsByClassName("light-color")[1].style.backgroundColor = lightColor;
-*/
-        $tell.setResultView();
-    },
-    setUpSiteView : function (siteLangNum){
-    	$(".1st-lang-select").val(siteLangNum);
-    	if (siteLangNum == 1) {
-    		$(".2nd-lang-select").val(0);
-    	}else{
-    		$(".2nd-lang-select").val(1);
-    	}
+     	$(".dark-color").css("backgroundColor" , darkColor);
+     	$(".light-color").css("backgroundColor" , lightColor);
 
-		$(".1st-lang-select option[value="+siteLangNum+"]").hide();
-    	$(".1st-lang-select option[value!="+siteLangNum+"]").show();
-    	$(".2nd-lang-select option[value="+siteLangNum+"]").hide();
-    	$(".2nd-lang-select option[value!="+siteLangNum+"]").show();
+     	$tell.setResultView();
+     },
+     setUpSiteView: function(siteLangNum) {
+     	$(".1st-lang-select").val(siteLangNum);
+     	if (siteLangNum == 1) {
+     		$(".2nd-lang-select").val(0);
+     	} else {
+     		$(".2nd-lang-select").val(1);
+     	}
 
-    	$tellLang.setSiteLang(siteLangNum);
-    },
-    init: function() {
+     	$(".1st-lang-select option[value=" + siteLangNum + "]").hide();
+     	$(".1st-lang-select option[value!=" + siteLangNum + "]").show();
+     	$(".2nd-lang-select option[value=" + siteLangNum + "]").hide();
+     	$(".2nd-lang-select option[value!=" + siteLangNum + "]").show();
+
+     	$tellLang.setSiteLang(siteLangNum);
+     },
+     init: function() {
         //색상 초기화
         $tell.setColor(0);
         //디자인 초기화
-        $("main .title-image").show();
-        $("header .title-image").hide();
+        $("main .title-image img").show();
+        $("header .title-image img").hide();
         //읽기 셀렉트 박스 초기화
         var siteLangNum = $tellLang.getSiteLang();
         $tell.setUpSiteView(siteLangNum);
-    }
+
+        $("#ENTER").val("");
+   }
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
 	$tell.bindAll();
-    $tellLang.bindAll();
+	$tellLang.bindAll();
 });
